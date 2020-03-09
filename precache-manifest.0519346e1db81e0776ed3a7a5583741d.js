ae2b88d83a54ce5724cd714282f0a1b92ac84daf
Nick Gulson <nicholasgulson2021@u.northwestern.edu>
2020-03-09 14:47:34 -0500

    Updates

---
+++ precache-manifest.0519346e1db81e0776ed3a7a5583741d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa74c8d94ae3bc40427eb98e1c1834ab",
+    "url": "/myapp/index.html"
+  },
+  {
+    "revision": "850d6509759e9be8a543",
+    "url": "/myapp/static/css/2.fc65253b.chunk.css"
+  },
+  {
+    "revision": "101a40ca0c99e4c5bd6e",
+    "url": "/myapp/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "850d6509759e9be8a543",
+    "url": "/myapp/static/js/2.0ce185f6.chunk.js"
+  },
+  {
+    "revision": "74b5b99a348fedd13ab1cb018bec8748",
+    "url": "/myapp/static/js/2.0ce185f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "101a40ca0c99e4c5bd6e",
+    "url": "/myapp/static/js/main.b7a70335.chunk.js"
+  },
+  {
+    "revision": "79fe458dc7993a217fff",
+    "url": "/myapp/static/js/runtime-main.5ff4a82b.js"
+  }
+]);
